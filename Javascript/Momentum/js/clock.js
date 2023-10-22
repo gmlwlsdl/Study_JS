eab5c4893f2e427f51006f04fae6b18c1306b172
gmlwlsdl <heejin094@naver.com>
2023-10-22 09:31:11 +0900

    add padStart

--- Javascript/Momentum/js/clock.js
+++ Javascript/Momentum/js/clock.js
@@ -3,7 +3,12 @@ const clock = document.querySelector("#clock");
 
 function setTime(){
     const date = new Date();
-    clock.innerText = (`${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`);
+    const hours = String(date.getHours()).padStart(2,"0");
+    const minutes = String(date.getMinutes()).padStart(2,"0");
+    const second = String(date.getSeconds()).padStart(2,"0");
+
+
+    clock.innerText = (`${hours}:${minutes}:${second}`);
 }
 
 setTime();
