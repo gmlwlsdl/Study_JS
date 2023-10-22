69eb502979f12b71b969fb066fe04133bc47ddb4
gmlwlsdl <heejin094@naver.com>
2023-10-22 11:05:01 +0900

    add backgroundImg

---
+++ Javascript/Momentum/js/background.js
@@ -0,0 +1,13 @@
+const images = [
+    "france.jpg",
+    "galaxy.jpg",
+    "hambrug.jpg"
+];
+
+const randImg = images[Math.floor(Math.random()*images.length)];
+
+const bgImg = document.createElement("img");
+
+bgImg.src=`img/${randImg}`;
+
+document.body.appendChild(bgImg);
